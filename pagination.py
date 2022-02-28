58b3d4d1c8640796466b34f7edc05c374a6a0b4d
easy-quest <esim.i2p@gmail.com>
2022-02-28 22:41:04 +0000

    GNU/Linux, Mon 28 Feb 2022 10:41:04 PM UTC

--- pagination.py
+++ pagination.py
@@ -1,16 +1,18 @@
 import httpx
-import requests
 from bs4 import BeautifulSoup
+from core.urlcore import *
 
 def scrape_teams(url: str) -> None:
     max_pages = 6
     current_page = 1
     
+    proxies = {"all": "socks5://127.0.0.1:9050"}
     while current_page <= max_pages:
         current_url = f'{url}?query=&amp;sort_direction=desc&amp;sort=rate&amp;page={current_page}'
         print(current_url)
         
-        raw_html = requests.get(current_url)
+        
+        raw_html = httpx.get(current_url,)
         
         print(raw_html.status_code)
         
@@ -20,9 +22,6 @@ def scrape_teams(url: str) -> None:
 
 def main() -> int:
     url = 'http://hydraclubbioknikokex7njhwuahc2l67lfiz7z36md2jvopda7nchid.onion/catalog/2'
-    # url = 'http://hydraclubbioknikokex7njhwuahc2l67lfiz7z36md2jvopda7nchid.onion/catalog/2'
-    # url = 'http://hydraclubbioknikokex7njhwuahc2l67lfiz7z36md2jvopda7nchid.onion/catalog/2?query=&amp;sort_direction=desc&amp;sort=rate&amp;page=2'
-    # url = 'http://hydraclubbioknikokex7njhwuahc2l67lfiz7z36md2jvopda7nchid.onion/catalog/2?query=&amp;sort_direction=desc&amp;sort=rate&amp;page=3'
     scrape_teams(url)
     return 0
 
